c725e0907efb45397913edd6243cfc61cf998bcf
Antonalo <xronosxronos@mail.ru>
2025-09-24 18:49:25 +0500

    Update#1

--- main.go
+++ main.go
@@ -1,30 +1,57 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Task struct {
-	ID   string `json:"id"`
-	Task string `json:"task"`
+	ID     string `gorm:"primaryKey" json:"id"`
+	Task   string `json:"task"`
+	IsDone bool   `json:"is_done"`
 }
 
 type taskRequest struct {
-	Task string `json:"task"`
+	Task   string `json:"task"`
+	IsDone bool   `json:"is_done"`
 }
 
 type UpdateTaskRequest struct {
-	Task string `json:"task"`
+	Task   *string `json:"task"`
+	IsDone *bool   `json:"is_done"`
 }
 
-var tasks []Task
+var db *gorm.DB
+
+func initDB() {
+	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
+
+	var err error
+
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+
+	if err := db.AutoMigrate(&Task{}); err != nil {
+		log.Fatalf("Could not migrate: %v", err)
+	}
+}
 
 func getTask(c echo.Context) error {
-	return c.JSON(http.StatusOK, tasks)
+	var task []Task
+
+	if err := db.Find(&task).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Could not get task"})
+	}
+
+	return c.JSON(http.StatusOK, task)
 }
 
 func postTask(c echo.Context) error {
@@ -38,39 +65,48 @@ func postTask(c echo.Context) error {
 	}
 
 	task := Task{
-		ID:   uuid.NewString(),
-		Task: req.Task,
+		ID:     uuid.NewString(),
+		Task:   req.Task,
+		IsDone: req.IsDone,
 	}
 
-	tasks = append(tasks, task)
+	if err := db.Create(&task).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Could not create task"})
+	}
 
 	return c.JSON(http.StatusCreated, task)
 }
 
 func updateTask(c echo.Context) error {
 	id := c.Param("id")
+
 	if _, err := uuid.Parse(id); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid task ID format"})
 	}
 
-	for i, task := range tasks {
-		if task.ID == id {
-			var req UpdateTaskRequest
-			if err := c.Bind(&req); err != nil {
-				return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid request"})
-			}
-
-			if req.Task == "" {
-				return c.JSON(http.StatusBadRequest, map[string]string{"error": "task text cannot be empty"})
-			}
-
-			tasks[i].Task = req.Task
-
-			return c.JSON(http.StatusOK, tasks[i])
-		}
+	var req UpdateTaskRequest
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid request"})
 	}
 
-	return c.JSON(http.StatusNotFound, map[string]string{"error": "task not found"})
+	var task Task
+	if err := db.First(&task, "id = ?", id).Error; err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Could not get task"})
+	}
+
+	if req.Task != nil {
+		task.Task = *req.Task
+	}
+
+	if req.IsDone != nil {
+		task.IsDone = *req.IsDone
+	}
+
+	if err := db.Save(&task).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Could not update task"})
+	}
+
+	return c.JSON(http.StatusOK, task)
 }
 
 func deleteTask(c echo.Context) error {
@@ -79,17 +115,21 @@ func deleteTask(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid task ID format"})
 	}
 
-	for i, task := range tasks {
-		if task.ID == id {
-			tasks = append(tasks[:i], tasks[i+1:]...)
-
-			return c.NoContent(http.StatusNoContent)
-		}
+	var task Task
+	if err := db.First(&task, "id = ?", id).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Could not delete task"})
 	}
-	return c.JSON(http.StatusNotFound, map[string]string{"error": "task not found"})
+
+	if err := db.Delete(&task, "id = ?", id).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Could not delete task"})
+	}
+
+	return c.NoContent(http.StatusNoContent)
 }
 
 func main() {
+	initDB()
+
 	e := echo.New()
 
 	e.Use(middleware.CORS())
